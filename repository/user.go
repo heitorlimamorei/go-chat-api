93c34e33858b57fa48df9e99c0c0f4708b39108c
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-02-21 17:15:45 -0300

    feat: add of User repository

---
+++ repository/user.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/heitorlimamorei/go-chat-api/schemas"
+)
+
+func GetUsers() ([]*schemas.User, error) {
+	var users []*schemas.User
+
+	err := db.Find(&users).Error
+
+	if err != nil {
+		logger.ErrorF("Error while getting users: %v", err)
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func GetUserByEmail(email string) (*schemas.User, error) {
+	var users []*schemas.User
+
+	err := db.Where("email =?", email).Find(users).Error
+
+	if err != nil {
+		logger.ErrorF("Error while getting users: %v", err)
+		return nil, err
+	}
+
+	if len(users) == 0 {
+		logger.ErrorF("User not found with(email): %v", email)
+		return nil, fmt.Errorf("user not found with email: %v", email)
+	}
+
+	return users[0], nil
+}
+
+func GetUserById(id string) (*schemas.User, error) {
+	user := schemas.User{}
+
+	err := db.Find(&user, id).Error
+
+	if err != nil {
+		logger.ErrorF("Error while getting user: %v", err)
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func CreateUser(newUser *schemas.User) error {
+	err := db.Create(newUser).Error
+
+	if err != nil {
+		logger.ErrorF("Error while creating user: %v", err)
+		return err
+	}
+	return nil
+}
+
+func UpdateUser(updatedUser *schemas.User, id string) error {
+	user, err := GetUserById(id)
+
+	if err != nil {
+		return err
+	}
+
+	user.Name = updatedUser.Name
+	user.Email = updatedUser.Email
+	user.Age = updatedUser.Age
+
+	db.Save(user)
+
+	return nil
+}
--- schemas/schemas.go
+++ schemas/schemas.go
@@ -36,4 +36,23 @@ type MessageResp struct {
 	ChatId    string    `json:"chatId"`
 	Text      string    `json:"text"`
 	Author    string    `json:"author"`
+	Chat      Chat      `gorm:"foreignKey:ChatId"`
+}
+
+type User struct {
+	gorm.Model
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+type UserResp struct {
+	gorm.Model
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	Email     string    `json:"email"`
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at,omitempty"`
 }
