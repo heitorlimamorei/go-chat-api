dbb6c5c6e510b0df0877d46ed5079e7bc9d1fddb
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-02-24 09:56:10 -0300

    chore: add env for port to run in cloud envs

--- router/router.go
+++ router/router.go
@@ -1,9 +1,19 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Init() {
 	router := gin.Default()
 	initializeRoutes(router)
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	router.Run(port)
 }
